2b720b5831fa1dbff3fabdd75df818a8ec164b31
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2020-02-25 00:54:23 -0500

    Create discipline
    It includes the endpoint to create a discipline and validates that name
    parameter is present always. It also includes unit test.

--- app/controllers/api/v1/disciplines_controller.rb
+++ app/controllers/api/v1/disciplines_controller.rb
@@ -3,7 +3,26 @@ class API::V1::DisciplinesController < ApplicationController
     render json: disciplines.to_json
   end
 
+  def create
+    discipline = Discipline.new(permited_params)
+    if discipline.save
+      render json: discipline.to_json
+    else
+      render json: message_error(discipline)
+    end
+  end
+
   private
+
+  def message_error(discipline)
+    {
+      error: discipline.errors.to_json
+    }
+  end
+
+  def permited_params
+    params.permit(:name)
+  end
 
   def disciplines
     Discipline.all
--- spec/controllers/api/v1/disciplines_controller_spec.rb
+++ spec/controllers/api/v1/disciplines_controller_spec.rb
@@ -12,4 +12,36 @@ RSpec.describe API::V1::DisciplinesController, type: :request do
       end
     end
   end
+
+  describe 'POST#create' do
+    let(:response_body) { JSON.parse(response.body) }
+
+    before { post api_v1_disciplines_path(params) }
+
+    context 'when the parameters are valid' do
+      let(:response_id) { response_body['id'] }
+      let(:params) do
+        {
+          name: 'Filosofia'
+        }
+      end
+
+      it 'returns the created object' do
+        expect(response_body).not_to be nil
+      end
+
+      it 'stored the object created' do
+        expect(JSON.parse(Discipline.find(response_id).to_json)).to eq(response_body)
+      end
+    end
+
+    context "when the parameters aren't valid" do
+      let(:params) { { } }
+
+      it 'returns the message error' do
+        expect(response_body).to include('error')
+        expect(response_body['error']).not_to be_empty
+      end
+    end
+  end
 end
